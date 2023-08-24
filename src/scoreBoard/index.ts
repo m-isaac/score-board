cb0fe920f6047cf1d01b58b4d23336ddf31adb88
Mina Isaac <isaac.mina.2010@gmail.com>
2023-08-24 02:17:37 +0200

    Implement updateMatch method

--- src/scoreBoard/index.ts
+++ src/scoreBoard/index.ts
@@ -6,10 +6,22 @@ type MatchId = `${string},${string}`;
 class ScoreBoard implements IScoreBoard {
   private matches: Map<MatchId, Scores> = new Map();
 
+  private verifyMatchExists = (matchId: MatchId) => {
+    if (!this.matches.has(matchId)) throw new Error('Match does not exists');
+  };
+
   startNewMatch = (homeTeam: string, awayTeam: string) => {
     const matchId: MatchId = `${homeTeam},${awayTeam}`;
     if (this.matches.has(matchId)) throw new Error('Match already started');
     this.matches.set(matchId, [0, 0]);
+    return this;
+  };
+
+  updateMatch = (teams: [string, string], scores: [number, number]) => {
+    const [homeTeam, awayTeam] = teams;
+    const matchId: MatchId = `${homeTeam},${awayTeam}`;
+    this.verifyMatchExists(matchId);
+    this.matches.set(matchId, scores);
     return this;
   };
 }
